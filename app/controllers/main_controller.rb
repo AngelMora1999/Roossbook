723cd2020cb91434a70713c0eb621fb10297009c
Angel Mora <eduardo.aemc@gmail.com>
2016-07-17 16:16:02 -0500

    Layout con herencias

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,4 +4,10 @@ class MainController < ApplicationController
 
   def unregistered
   end
+
+  protected
+  def set_layout
+  	return "landing" if action_name == "unregistered"  
+  	super
+  end
 end
